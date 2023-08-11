4e4512514b43baa92239c473d81536de28a7fe43
sammydepoj <samueladepoju86@yahoo.com>
2023-08-11 13:10:41 +0100

    folder skeleton for some pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { Route, Routes } from "react-router-dom";
+import Sigin from "./Pages/Signin/Sigin";
+import SignUp from "./Pages/SignUp/SignUp";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 function App() {
   return (
-    <>
-      <div className=" new w-[30rem] bg-[blue] h-32">HomePages</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Sigin />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+// import StudentDetails from "./components/StudentDetails";
+const Dashboard = () => {
+  return (
+    <div>
+      dashboard
+      {/* <StudentDetails /> */}
+    </div>
+  );
+};
+
+export default Dashboard;
