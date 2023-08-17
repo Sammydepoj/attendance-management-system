aeb0e94bc9c4fe3ab487c9ad83a0d5378f700400
sammydepoj <samueladepoju86@yahoo.com>
2023-08-17 13:14:45 +0100

    navigation to dashboard after login implemneted

--- src/Pages/DashboardNew/components/LogoutButton.jsx
+++ src/Pages/DashboardNew/components/LogoutButton.jsx
@@ -1,18 +1,18 @@
 import { BiLogOut } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 
-
 const LogoutButton = () => {
-    const Navigate = useNavigate()
+  const navigate = useNavigate();
   const logoutHandler = () => {
-        localStorage.removeItem("token");
-        Navigate("/", {
-        replace: true,
+    localStorage.removeItem("token");
+    navigate("/", {
+      replace: true,
     });
-};
+  };
   return (
     <div>
-      <button className="flex items-center mt-[5.5rem] gap-4 p-4  text-slate-400 text-xl "
+      <button
+        className="flex items-center mt-[5.5rem] gap-4 p-4  text-slate-400 text-xl "
         onClick={logoutHandler}
       >
         <BiLogOut />
