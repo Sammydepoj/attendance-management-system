11934c4091e3e7ec24dec27a8dd7352e5f2e2bef
Sammydepoj <samueladepoju86@yahoo.com>
2023-08-23 22:32:57 +0100

    backend testin for clock in history

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://ams-backend-yjri.onrender.com/";
-// export const BASE_URL = "http://localhost:5000/";
+// export const BASE_URL = "https://ams-backend-yjri.onrender.com/";
+export const BASE_URL = "http://localhost:5000/";
